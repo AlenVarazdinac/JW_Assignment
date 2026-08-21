b3fa122f2d81eff7dcfc8e781d47ce0ad7d8d378
Alen Varazdinac <alen.varazdinac@gmail.com>
2026-08-21 22:19:04 +0200

    Navbar

---
+++ app/layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="min-h-screen bg-gray-50">
+    <header class="flex items-center gap-1 border-b border-gray-200 bg-white px-7 py-6">
+      <Icon
+        name="custom:logo"
+        class="size-6 text-primary-700"
+      />
+      <span class="font-plus-jakarta text-[14px] leading-5.75 font-bold text-primary-800">Visa Wizard</span>
+    </header>
+
+    <main class="p-8">
+      <slot />
+    </main>
+  </div>
+</template>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="min-h-screen bg-gray-50 p-8">
+  <div>
     <VisaWizard class="mx-auto mb-16 w-220" />
 
     <div class="mx-auto mt-6 w-220">
